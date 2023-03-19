70f904bcc8c67656697bc4a1559f3ecc2fce4e9c
Bijay Shrestha <100466191+BijayShrestha78@users.noreply.github.com>
2023-03-19 22:06:24 +0545

    Update currency.java

--- currency.java
+++ currency.java
@@ -1,14 +1,20 @@
-import java.util.Scanner;
-public class currency 
-{
-    public static void main(String[] args) 
-    {
 
-        Scanner b = new Scanner(System.in);
-        System.out.printf("Enter the  Nepalese  rupaiya you want to enter ");
-        double nepaliRup = b.nextDouble();
-        double usDoll = nepaliRup / 135.03;
-        System.out.printf("%.2f Nepali Rup = %.2f US Doll\n", nepaliRup, usDoll);
-        
+import java.util.Scanner;
+
+public class CurrencyConverter {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the amount in Nepalese rupaiya: ");
+
+        if (input.hasNextDouble()) {
+            double nepaliRup = input.nextDouble();
+            double usDoll = nepaliRup / 118.50;
+            System.out.printf("%.2f Nepalese rupaiya = %.2f US dollars\n", nepaliRup, usDoll);
+        } else {
+            System.out.println("Invalid input. Please enter a number.");
+        }
+
+        input.close();
     }
 }
