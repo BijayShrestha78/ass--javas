e52be61732be72a45cde63bfe79f3c3ecdfa5e71
Bijay Shrestha <100466191+BijayShrestha78@users.noreply.github.com>
2023-03-19 21:59:43 +0545

    Update Permutation.java

--- Permutation.java
+++ Permutation.java
@@ -3,20 +3,19 @@ import java.util.Arrays;
 public class Permutation {
     public static void main(String[] args) {
         int[] nums1 = {0, 2, 1, 5, 3, 2};
-        int[] ans1 = buildArray(nums1);
+        int[] ans1 = new int[nums1.length];
+        buildArray(nums1, ans1);
         System.out.println(Arrays.toString(ans1));
         
         int[] nums2 = {5, 0, 1, 2, 3, 5};
-        int[] ans2 = buildArray(nums2);
+        int[] ans2 = new int[nums2.length];
+        buildArray(nums2, ans2);
         System.out.println(Arrays.toString(ans2));
     }
     
-    public static int[] buildArray(int[] nums) {
-        int n = nums.length;
-        int[] ans = new int[n];
-        for (int i = 0; i < n; i++) {
+    public static void buildArray(int[] nums, int[] ans) {
+        for (int i = 0; i < nums.length; i++) {
             ans[i] = nums[nums[i]];
         }
-        return ans;
     }
 }
